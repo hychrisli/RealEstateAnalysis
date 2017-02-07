160b1070f62542d5ac8c88abca2db8192f2d62e9
Chris Li <kittokevin@gmail.com>
2017-02-06 16:45:00 -0800

    Scrawl lists

--- real_estate/spiders/mlslistings_spider.py
+++ real_estate/spiders/mlslistings_spider.py
@@ -1,3 +1,34 @@
-
-
+import scrapy
+import re
+# This is a spider for
 # http://www.mlslistings.com/browse-listings#/
+
+
+class MlsSpider(scrapy.Spider):
+    name = "mls"
+
+    def start_requests(self):
+        url = 'http://www.mlslistings.com/browse-listings#/'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        links = response.xpath('//ul[@class="BrowseMenu"]/li/a/@href').extract()
+
+        if not links:
+            return
+        else:
+            for href in links:
+                if 'Southern' not in href:
+                    print(href)
+                    # yield scrapy.Request(url=href, callback=self.parse)
+
+
+    def parse_county_page(self, response):
+        return
+
+    def parse_city_page(self):
+        return
+
+
+
+
