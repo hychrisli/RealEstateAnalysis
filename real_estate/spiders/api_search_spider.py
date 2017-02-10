c3ebf9850731451d63d514c36174c9b5cd505fd8
Chris Li <kittokevin@gmail.com>
2017-02-10 15:08:56 -0800

    Success fully parse response from api.mlslistings.com

--- real_estate/spiders/api_search_spider.py
+++ real_estate/spiders/api_search_spider.py
@@ -1,29 +1,40 @@
-from __future__ import print_function
 import scrapy
+import json
 from scrapy_splash import SplashRequest
-from lxml import etree
 
 
 class ApiSearchSpider(scrapy.Spider):
     name = 'api_search'
 
     def start_requests(self):
-        url = 'http://api.mlslistings.com'
+        url = 'http://api.mlslistings.com/api/widgetsearch'
         post_json = '{"display":{"pageNumber":1,"itemsPerPage":200},"cityName":"",' \
                     '"countyName":"","zipCode":"94086","mlsNumber":"","address":"",' \
                     '"beds":"","baths":"","listSalePrice":""}'
+        header = {'Accept': 'application/json, text/plain, */*',
+                  'Accept-Encoding': 'gzip, deflate',
+                  'Accept-Language': 'en-US,en;q=0.5',
+                  'Connection': 'keep-alive',
+                  'Content-Length': '166',
+                  'Content-Type': 'application/json;charset=utf-8',
+                  'Host': 'api.mlslistings.com',
+                  'Referer': 'http://api.mlslistings.com/',
+                  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
+                  'X-Requested-With': 'XMLHttpRequest'}
+
         yield SplashRequest(url, self.parse,
-                            endpoint='render.json',
+                            headers=header,
                             args={'wait': 0.5,
                                   'http_method': 'POST',
                                   'body': post_json})
 
     def parse(self, response):
-        nodes = response.xpath('//div[@class="ng-scope"]').extract()
-        f = open('f.html', 'w')
-        #print('Number of listings: ' + str(len(nodes)))
-        print(response.data, file=f)
-        f.close()
-        # for node in nodes:
-        #     tree = etree.HTML(node)
-        #     print(tree.xpath('//span[@class="longAddress ng-binding"]/text()')[0])
+        body = response.body
+        html_selectors_before = '<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">'
+        html_selectors_after = '</pre></body></html>'
+        res_str = body.replace(html_selectors_before, '').replace(html_selectors_after, '')
+        res_json = json.loads(res_str)
+        res_lst = res_json['propertySearchResults']
+
+        for item in res_lst:
+            print (item['MLSNumber'] + ' : ' + item['filteredAddress'])
