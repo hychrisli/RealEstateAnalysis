4f1c8e9010667dd170168518cd8424242014a188
Chris Li <kittokevin@gmail.com>
2017-02-13 15:34:13 -0800

    MySql insert

--- real_estate/mysql/zipcode_dao.py
+++ real_estate/mysql/zipcode_dao.py
@@ -8,14 +8,27 @@ class MySqlConnector:
         db_user = os.environ['DB_USER']
         db_pass = os.environ['DB_PASS']
         db_host = os.environ['DB_HOST']
+        database = os.environ['DATABASE']
         config = {
             'user': db_user,
             'password': db_pass,
-            'host': db_host}
+            'host': db_host,
+            'database': database
+        }
 
         self.cnx = mysql.connector.connect(**config)
+        self.cursor = self.cnx.cursor()
+
+    def add_county(self, county):
+
+        insert_stmt = "INSERT INTO COUNTY (NAME) VALUES ( %(county)s )"
+        value = {'county': "\'" + county + "\'"}
+        self.cursor.execute(insert_stmt, value)
+        self.cnx.commit()
+        print(self.cursor.lastrowid)
 
     def close(self):
+        self.cursor.close()
         self.cnx.close()
 
 
--- real_estate/spiders/zipcode_spider.py
+++ real_estate/spiders/zipcode_spider.py
@@ -1,25 +1,38 @@
 import scrapy
+from lxml import etree
+from ..mysql.zipcode_dao import MySqlConnector
 # This is a spider for
 # http://www.mlslistings.com/browse-listings#/
 
 
-class MlsSpider(scrapy.Spider):
+class ZipCodeSpider(scrapy.Spider):
     name = "zipcode"
+
+    def __init__(self):
+        super(ZipCodeSpider, self).__init__()
+        self.connector = MySqlConnector()
 
     def start_requests(self):
         url = 'http://www.mlslistings.com/browse-listings#/'
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        links = response.xpath('//ul[@class="BrowseMenu"]/li/a/@href').extract()
+        nodes = response.xpath('//ul[@class="BrowseMenu"]/li/a').extract()
 
-        if not links:
+        # links = response.xpath('//ul[@class="BrowseMenu"]/li/a/@href').extract()
+        if not nodes:
             return
         else:
-            for href in links:
+            for node in nodes:
+                tree = etree.HTML(node)
+                name = tree.xpath('//a/text()')[0]
+                href = tree.xpath('//a/@href')[0]
                 if 'Southern' not in href:
+                    print(name)
                     print(href)
+                    self.connector.add_county(name)
                     # yield scrapy.Request(url=href, callback=self.parse)
+        # self.connector.close()
 
 
     def parse_county_page(self, response):
