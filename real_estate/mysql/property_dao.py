4eb6e7fd3c19d00396e8390857197fa4b2735e73
Chris Li <kittokevin@gmail.com>
2017-02-14 15:17:56 -0800

    Multiple inserts

--- real_estate/mysql/property_dao.py
+++ real_estate/mysql/property_dao.py
@@ -31,8 +31,7 @@ class PropertyConnector:
         self.cursor.execute(delete_stmt)
         self.cursor.execute(reset_stmt)
 
-    def add_property(self, property):
-
+    def add_properties(self, property_lst):
         insert_stmt = "INSERT INTO PROPERTY_STG " \
                       "(MLS_ID, ZIPCODE, CITY, ADDR, BEDS, FULL_BATHS, PART_BATHS, STRUCT_SQFT, " \
                       "LOT_SIZE, LOT_SIZE_UNIT, YEAR_BUILT, LIST_PRICE, LIST_STATUS, URL) " \
@@ -40,36 +39,34 @@ class PropertyConnector:
                       "( %(mls_id)s, %(zipcode)s, %(city)s, %(addr)s, %(beds)s, %(full_baths)s, " \
                       "%(part_baths)s, %(struct_sqft)s, %(lot_size)s, %(lot_size_unit)s, %(year_built)s, " \
                       "%(list_price)s, %(list_status)s, %(url)s)"
+        values = []
+        for prop in property_lst:
+            values.append(PropertyConnector.__gen_insert_value__(prop))
 
-        value = {'mls_id':  property.mls_id,
-                 'zipcode': property.zipcode,
-                 'city': property.city,
-                 'addr': property.addr,
-                 'beds': property.beds,
-                 'full_baths': property.full_baths,
-                 'part_baths': property.part_baths,
-                 'struct_sqft': PropertyConnector.__handle_nan__(property.struct_sqft),
-                 'lot_size': PropertyConnector.__handle_nan__(property.lot_size),
-                 'lot_size_unit': property.lot_size_unit,
-                 'year_built': property.year_built,
-                 'list_price': PropertyConnector.__handle_nan__(property.list_price),
-                 'list_status': property.list_status,
-                 'url': property.url
-                 }
-
-        return self.__exec_insert__(insert_stmt, value)
+        self.cursor.executemany(insert_stmt, values)
 
     def close(self):
         self.cursor.close()
         self.cnx.close()
 
-    def __exec_insert__(self, insert_stmt, value):
-        self.cursor.execute(insert_stmt, value)
-        return self.cursor.getlastrowid()
-
-    def __exec_single_select__(self, select_stmt, value):
-        self.cursor.execute(select_stmt, value)
-        return self.cursor.fetchone()[0]
+    @staticmethod
+    def __gen_insert_value__(prop):
+        value = {'mls_id': prop.mls_id,
+                 'zipcode': prop.zipcode,
+                 'city': prop.city,
+                 'addr': prop.addr,
+                 'beds': prop.beds,
+                 'full_baths': prop.full_baths,
+                 'part_baths': prop.part_baths,
+                 'struct_sqft': PropertyConnector.__handle_nan__(prop.struct_sqft),
+                 'lot_size': PropertyConnector.__handle_nan__(prop.lot_size),
+                 'lot_size_unit': prop.lot_size_unit,
+                 'year_built': prop.year_built,
+                 'list_price': PropertyConnector.__handle_nan__(prop.list_price),
+                 'list_status': prop.list_status,
+                 'url': prop.url
+                 }
+        return value
 
     @staticmethod
     def __handle_nan__(num_float):
--- real_estate/spiders/api_search_spider.py
+++ real_estate/spiders/api_search_spider.py
@@ -8,26 +8,16 @@ from ..mysql.property_dao import PropertyConnector
 class ApiSearchSpider(scrapy.Spider):
     name = 'api_search'
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         super(ApiSearchSpider, self).__init__()
         self.connector = PropertyConnector()
+        self.zipcode = kwargs.get('zipcode')
 
     def start_requests(self):
         url = 'http://api.mlslistings.com/api/widgetsearch'
-        post_json = '{"display":{"pageNumber":1,"itemsPerPage":200},"cityName":"",' \
-                    '"countyName":"","zipCode":"94086","mlsNumber":"","address":"",' \
-                    '"beds":"","baths":"","listSalePrice":""}'
-        header = {'Accept': 'application/json, text/plain, */*',
-                  'Accept-Encoding': 'gzip, deflate',
-                  'Accept-Language': 'en-US,en;q=0.5',
-                  'Connection': 'keep-alive',
-                  'Content-Length': '166',
-                  'Content-Type': 'application/json;charset=utf-8',
-                  'Host': 'api.mlslistings.com',
-                  'Referer': 'http://api.mlslistings.com/',
-                  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
-                  'X-Requested-With': 'XMLHttpRequest'}
-
+        # zipcodes = ['93907', '93901', '93908']
+        header = ApiSearchSpider.__gen_header__()
+        post_json = ApiSearchSpider.__gen_post_json__(self.zipcode)
         yield SplashRequest(url, self.parse,
                             headers=header,
                             args={'wait': 0.5,
@@ -41,9 +31,30 @@ class ApiSearchSpider(scrapy.Spider):
         res_str = body.replace(html_selectors_before, '').replace(html_selectors_after, '')
         res_json = json.loads(res_str)
         res_lst = res_json['propertySearchResults']
+        property_lst = []
 
         for item in res_lst:
-            list_property = ListProperty(item)
-            list_property.print_details()
-            self.connector.add_property(list_property)
-            # print (item['MLSNumber'] + ' : ' + item['filteredAddress'])
+            prop = ListProperty(item)
+            prop.print_details()
+            property_lst.append(prop)
+
+        self.connector.add_properties(property_lst)
+
+    @staticmethod
+    def __gen_header__():
+        return {'Accept': 'application/json, text/plain, */*',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Connection': 'keep-alive',
+                'Content-Length': '166',
+                'Content-Type': 'application/json;charset=utf-8',
+                'Host': 'api.mlslistings.com',
+                'Referer': 'http://api.mlslistings.com/',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
+                'X-Requested-With': 'XMLHttpRequest'}
+
+    @staticmethod
+    def __gen_post_json__(zipcode):
+        return '{"display":{"pageNumber":1,"itemsPerPage":200},"cityName":"",' \
+               '"countyName":"","zipCode":"' + zipcode + '","mlsNumber":"","address":"",' \
+               '"beds":"","baths":"","listSalePrice":""}'
