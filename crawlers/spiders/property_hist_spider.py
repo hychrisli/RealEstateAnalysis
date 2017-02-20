7cd64963bd8eade81a77b37ea2f4d3bc5848d136
Chris Li <kittokevin@gmail.com>
2017-02-20 14:27:59 -0800

    Entity prop_hist

--- crawlers/spiders/property_hist_spider.py
+++ crawlers/spiders/property_hist_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 from lxml import etree
+from entities.prop_hist import PropHist
+
 
 class PropHistSpider(scrapy.Spider):
     name = "prop_hist"
@@ -10,10 +12,15 @@ class PropHistSpider(scrapy.Spider):
 
     def parse(self, response):
         rows = response.xpath('//div[@id="ldp-history-price"]//tbody/tr').extract()
+        prop_hist_lst = []
 
         for row in rows:
             cols = etree.HTML(row)
+            prop_hist = PropHist()
+            prop_hist.date = cols.xpath('//td[1]/text()')[0]
+            prop_hist.event = cols.xpath('//td[2]/text()')[0]
+            prop_hist.set_price(cols.xpath('//td[3]/text()')[0])
+            prop_hist.set_price_sqft(cols.xpath('//td[4]/text()')[0])
 
-            date = cols.xpath('//td[1]/text()')[0]
-
-            print(date)
+            prop_hist.print_hist()
+            prop_hist_lst.append(prop_hist)
