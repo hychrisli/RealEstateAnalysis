ad8c242efd3201320c1d7e9ddb0052ba179ac3d7
Chris Li <kittokevin@gmail.com>
2017-02-20 17:55:23 -0800

    Initial load. USPS Address

--- db_ops/mysql_dao/addr_dao.py
+++ db_ops/mysql_dao/addr_dao.py
@@ -4,7 +4,7 @@ from pyusps.address_information import OrderedDict
 
 class AddrDao(GenericConnector):
     MLS_ADDR_VIEW = 'v_mls_addr'
-    ADDR_PROP_TAB = 'addr_prop_fact'
+    ADDR_PROP_TAB = 'prop_addr_fact'
     REQ_SIZE = 5
 
     def init_cleanup(self):
@@ -13,18 +13,34 @@ class AddrDao(GenericConnector):
     def add_addrs(self):
 
         verf = AddrVerfEtl()
-
+        insert_stmt = "INSERT INTO " + AddrDao.ADDR_PROP_TAB + \
+                      " (MLS_ID, AREA_ID, USPS_ADDR) VALUES (%(mls_id)s, %(area_id)s, %(addr)s)"
         select_stmt = "SELECT MLS_ID, AREA_ID, ADDR, CITY, STATE FROM " + AddrDao.MLS_ADDR_VIEW
+
         addrs = self.__select_all__(select_stmt)
-        (id_lst, addr_dict_lst) = self.__build_addr_dict_lst__(addrs)
-        res_lst = verf.verify(addr_dict_lst)
-        self.__parse_res_lst__(id_lst, res_lst)
+        addr_num = len(addrs)
+        req_num = addr_num / AddrDao.REQ_SIZE
+        insert_values = []
+
+        for i in range(0, req_num):
+            start = i * AddrDao.REQ_SIZE
+            end = (i + 1) * AddrDao.REQ_SIZE
+
+            if addr_num < end:
+                end = addr_num
+
+            print (start)
+            (id_lst, addr_dict_lst) = self.__build_addr_dict_lst__(addrs[start:end])
+            res_lst = verf.verify(addr_dict_lst)
+            insert_values.extend(self.__parse_res_lst__(id_lst, res_lst))
+
+        return self.cursor.executemany(insert_stmt, insert_values)
 
     def __build_addr_dict_lst__(self, addrs):
         addr_dict_lst = []
         id_lst = []
 
-        for (mls_id, area_id, addr, city, state) in addrs[0:AddrDao.REQ_SIZE]:
+        for (mls_id, area_id, addr, city, state) in addrs:
             addr_dict = dict([
                 ('address', addr),
                 ('city', city),
@@ -35,14 +51,12 @@ class AddrDao(GenericConnector):
         return id_lst, addr_dict_lst
 
     def __parse_res_lst__(self, id_lst, res_lst):
-
+        values = []
         for i in range(0, AddrDao.REQ_SIZE):
-            if isinstance(res_lst[i], ValueError):
-                print (res_lst[i])
-            elif isinstance(res_lst[i], OrderedDict):
-                print (res_lst[i])
-                value = self.__gen_insert_value_(id_lst[i], res_lst[i])
-                print (value)
+            if isinstance(res_lst[i], OrderedDict):
+                values.append(self.__gen_insert_value_(id_lst[i], res_lst[i]))
+        return values
+
 
     def __gen_insert_value_(self, ids, res):
         value = {
