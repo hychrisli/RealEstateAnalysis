8a237215c6c74499b7b4d4c5879041cbd163a3b1
Chris Li <kittokevin@gmail.com>
2017-02-25 11:59:03 -0800

    runner script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+echo "Starting Docker with password: "
+sudo systemctl start docker &
+wait $!
 
+if [ $? = 0 ]
+then 
+	echo "Starting Splash"
+	sudo docker run -d -p 8050:8050 scrapinghub/splash &
+	SPLASH_PID=`ps -ef | pgrep -f splash`
+
+	python main.py
+	
+	sudo kill ${SPLASH_PID}
+	echo "Splash stopped"
+fi
+
+sudo systemctl stop docker
+echo "Docker is stopped"
