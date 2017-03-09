be62c585554cd399a12071ea1cff977dedb49806
Chris Li <kittokevin@gmail.com>
2017-03-09 11:27:36 -0800

    prop_addr_price_rpt_etl

--- etl/routines/prop_addr_price_rpt_etl.py
+++ etl/routines/prop_addr_price_rpt_etl.py
@@ -1,25 +1,42 @@
 from ..abstr_cnx import GenericConnector
-from utility.actions import show_progress
+from utility.actions import show_progress, get_hms
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import time
 
 
 class PropAddrPriceRptEtl(GenericConnector):
-    PROP_ADDR_PRICE_RPT_TAB = 'prop_addr_price_rpt'
+    PROP_ADDR_PRICE_MONTH_RPT_TAB = 'prop_addr_price_month_rpt'
+    PROP_ADDR_PRICE_DAY_RPT_STG_TAB = 'prop_addr_price_day_rpt_stg'
+    START_DATE = datetime.strptime('2010-01-01', '%Y-%m-%d')
+    END_DATE = datetime.strptime('2017-03-01', '%Y-%m-%d') # Not included
+    END_MONTH = END_DATE
 
     def run(self):
-        self.__init_clean__()
-        start_date = datetime.strptime('2015-01-01', '%Y-%m-%d')
-        end_date = datetime.today() - + timedelta(days=7)
-        tot_days = (end_date - start_date).days + 1
-        date = start_date
-        while date <= end_date:
-            self.__call_sp_prop_addr_price_rpt__(date.strftime('%Y-%m-%d'))
-            done_days = (date - start_date).days + 1
-            show_progress(done_days, tot_days, 15, 'Processed: ' + date.strftime('%Y-%m-%d') + '\n')
-            date += timedelta(days=1)
 
-    def __init_clean__(self):
-        self.__clean_table__(PropAddrPriceRptEtl.PROP_ADDR_PRICE_RPT_TAB)
+        self.__clean_table__(PropAddrPriceRptEtl.PROP_ADDR_PRICE_MONTH_RPT_TAB)
+        start_time = time.time()
 
-    def __call_sp_prop_addr_price_rpt__(self, date_str):
-        self.cursor.execute('CALL sp_prop_addr_price_rpt(\'' + date_str + '\')')
+        month = PropAddrPriceRptEtl.START_DATE
+        tot_days = (PropAddrPriceRptEtl.END_DATE - PropAddrPriceRptEtl.START_DATE).days
+        date = PropAddrPriceRptEtl.START_DATE
+
+        while month < PropAddrPriceRptEtl.END_MONTH:
+            print("Month: " + month.strftime('%Y-%m') + ' | Time elapsed: ' + get_hms(time.time() - start_time))
+            self.__clean_table__(PropAddrPriceRptEtl.PROP_ADDR_PRICE_DAY_RPT_STG_TAB)
+            next_month = month + relativedelta(months=+1)
+
+            while date < next_month:
+                self.__call_sp_prop_addr_price_day_rpt_stg__(date.strftime('%Y-%m-%d'))
+                date += timedelta(days=1)
+
+            done_days = (date - PropAddrPriceRptEtl.START_DATE).days + 1
+            show_progress(done_days, tot_days, 1, '\n')
+            self.__call_sp_prop_addr_price_month_rpt__()
+            month = next_month
+
+    def __call_sp_prop_addr_price_day_rpt_stg__(self, date_str):
+        self.cursor.execute('CALL sp_prop_addr_price_day_rpt_stg(\'' + date_str + '\')')
+
+    def __call_sp_prop_addr_price_month_rpt__(self):
+        self.cursor.execute('CALL sp_prop_addr_price_month_rpt')
--- utility/actions.py
+++ utility/actions.py
@@ -1,5 +1,4 @@
 from random import randint
-
 import time
 
 
@@ -8,13 +7,18 @@ def show_progress(cur_num, tot_num, intvl, msg=''):
         if cur_num > tot_num:
             cur_num = tot_num
 
-        if cur_num % intvl == 0:
+        if cur_num % intvl == 0 or cur_num == tot_num:
             percent = round(float(cur_num) * 100.0 / float(tot_num), 2)
             print (str(percent) + "% finished. " + msg)
-
-        return True
+            return True
 
     return False
+
+
+def get_hms(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "%d:%02d:%02d" % (h, m, s)
 
 
 def rand_wait(msg, min_wait=10, max_wait=30):
