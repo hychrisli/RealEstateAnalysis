818abd7c49b9238b4d15216d5f687cbcbe0aa731
Chris Li <kittokevin@gmail.com>
2017-04-08 17:30:34 -0700

    Exception Response and Notification handling

--- utility/actions.py
+++ utility/actions.py
@@ -1,5 +1,7 @@
 from random import randint
 import time
+import httplib, urllib
+import os
 
 
 def show_progress(cur_num, tot_num, intvl, msg=''):
@@ -25,3 +27,28 @@ def rand_wait(msg, min_wait=10, max_wait=30):
     wait_time = randint(min_wait, max_wait)
     print (str(msg) + " | Waiting " + str(wait_time) + "s ...")
     time.sleep(wait_time)
+
+
+def except_response(e):
+    print(e)
+    print("Exception thrown. Continue? Y/N ")
+
+    valid = {"yes": True, "y": True, "ye": True}
+    choice = raw_input().lower()
+    if choice in valid:
+        return
+    else:
+        exit(0)
+
+
+def push_notification(msg):
+    token = os.environ['PUSHOVER_APP_TOKEN']
+    user = os.environ['PUSHOVER_USER_KEY']
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+                 urllib.urlencode({
+                     "token": token,
+                     "user": user,
+                     "message": msg,
+                 }), {"Content-type": "application/x-www-form-urlencoded"})
+    conn.getresponse()
